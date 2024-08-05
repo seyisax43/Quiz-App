78037dc83ce034e9860819bcb0555d2e8ad23559
seyisax43 <seyisax43@gmail.com.com>
2024-08-05 12:47:47 +0100

    first commit

---
+++ Quiz-App/public/assets/Data.jsx
@@ -0,0 +1,59 @@
+
+export const data = [
+  {
+    question: "What is the capital of France?",
+    
+    option1: "Berlin",
+    option2: "Madrid",
+    option3: "Paris",
+    option4: "Rome",
+      ans: 3,
+    },
+   
+ 
+  {
+    question: "Which planet is known as the Red Planet?",
+ 
+      option1: "Earth",
+      option2: "Mars",
+      option3: "Jupiter",
+      option4: "Venus",
+       ans: 2,
+    },
+   
+
+  {
+    question: "Who wrote 'To Kill a Mockingbird'?",
+   
+      option1: "Harper Lee",
+      option2: "Mark Twain",
+      option3: "Ernest Hemingway",
+      ans: 1,
+    },
+    
+
+  {
+    question: "What is the largest ocean on Earth?",
+  
+      option1: "Atlantic Ocean",
+      option2: "Indian Ocean",
+      option3: "Arctic Ocean",
+      option4: "Pacific Ocean",
+       ans: 4,
+    },
+   
+ 
+  {
+    question: "What is the smallest prime number?",
+ 
+      option1: "0",
+      option2: "1",
+      option3: "2",
+      option4: "3",
+        ans: 3,
+    },
+  
+
+];
+
+
---
+++ Quiz-App/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Quiz from './components/quiz/Quiz'
+import './App.css'
+
+function App() {
+ 
+  return (
+   
+      <div>
+      <Quiz />
+      <div className="copyright">&copy; SeyiScope 2024</div>
+      </div>
+     
+ 
+  )
+}
+
+export default App
